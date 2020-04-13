f6916a0b0a96b9d68f870e57ebc18097f2b4ff37
Rocket47 <romtswork@gmail.com>
2020-04-13 14:15:34 +0300

    Update folders. Initial commit for TreeSize project

---
+++ TreeSize/TreeSize/TreeSize/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TreeSize
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {       
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillDiscsInfo();
+        }
+
+        public void FillDiscsInfo()
+        {
+            int count = 0;
+            TreeView treeView = TreeViewMain;
+            TreeViewItem treeViewItem = TreeViewItem;
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            TreeViewItem itemsTreeView = (TreeViewItem)treeView.Items[0];
+            for (int i = 0; i < allDrives.Length; i++)
+            {
+                TreeViewItem newChild = new TreeViewItem();
+                treeViewItem.Items.Add(newChild);
+            }
+           
+            foreach (DriveInfo disc in allDrives)
+            {                
+                TreeViewItem childItem = (TreeViewItem)itemsTreeView.Items[count];
+                if (disc.IsReady == true)
+                {
+                    childItem.Header = disc.Name;                   
+                }
+                count++;
+            }
+        }
+
+        public void FillDirectoryInfo()
+        {
+            int count = 0;
+            TreeView treeView = TreeViewMain;
+            TreeViewItem treeViewItem = TreeViewItem;
+            DriveInfo[] allDrives = DriveInfo.GetDrives();
+            TreeViewItem itemsTreeView = (TreeViewItem)treeView.Items[0];
+            for (int i = 0; i < allDrives.Length; i++)
+            {
+                TreeViewItem newChild = new TreeViewItem();
+                treeViewItem.Items.Add(newChild);
+            }
+
+            foreach (DriveInfo disc in allDrives)
+            {
+                TreeViewItem childItem = (TreeViewItem)itemsTreeView.Items[count];
+                if (disc.IsReady == true)
+                {
+                    childItem.Header = disc.Name;
+                }
+                count++;
+            }
+        }
+    }
+}
